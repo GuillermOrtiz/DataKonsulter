0058a8a6dcb9732746d46e87ade6cd732ca22263
Pablo Alejandro Salazar Santizo <salazarpabloale@gmail.com>
2018-02-19 10:54:29 -0600

    Add files via upload

---
+++ Navegador1/Navegador1/Navegador.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Navegador1
+{
+    partial class Navegador
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNext2 = new System.Windows.Forms.Button();
+            this.btnNext1 = new System.Windows.Forms.Button();
+            this.btnLast1 = new System.Windows.Forms.Button();
+            this.btnLast2 = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNext2
+            // 
+            this.btnNext2.Image = global::Navegador1.Properties.Resources.next1;
+            this.btnNext2.Location = new System.Drawing.Point(570, 3);
+            this.btnNext2.Name = "btnNext2";
+            this.btnNext2.Size = new System.Drawing.Size(57, 54);
+            this.btnNext2.TabIndex = 9;
+            this.btnNext2.UseVisualStyleBackColor = true;
+            // 
+            // btnNext1
+            // 
+            this.btnNext1.Image = global::Navegador1.Properties.Resources.next;
+            this.btnNext1.Location = new System.Drawing.Point(507, 3);
+            this.btnNext1.Name = "btnNext1";
+            this.btnNext1.Size = new System.Drawing.Size(57, 54);
+            this.btnNext1.TabIndex = 8;
+            this.btnNext1.UseVisualStyleBackColor = true;
+            // 
+            // btnLast1
+            // 
+            this.btnLast1.Image = global::Navegador1.Properties.Resources.last1;
+            this.btnLast1.Location = new System.Drawing.Point(381, 3);
+            this.btnLast1.Name = "btnLast1";
+            this.btnLast1.Size = new System.Drawing.Size(57, 54);
+            this.btnLast1.TabIndex = 7;
+            this.btnLast1.UseVisualStyleBackColor = true;
+            // 
+            // btnLast2
+            // 
+            this.btnLast2.Image = global::Navegador1.Properties.Resources.last2;
+            this.btnLast2.Location = new System.Drawing.Point(444, 3);
+            this.btnLast2.Name = "btnLast2";
+            this.btnLast2.Size = new System.Drawing.Size(57, 54);
+            this.btnLast2.TabIndex = 6;
+            this.btnLast2.UseVisualStyleBackColor = true;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Image = global::Navegador1.Properties.Resources.print;
+            this.btnPrint.Location = new System.Drawing.Point(318, 3);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(57, 54);
+            this.btnPrint.TabIndex = 5;
+            this.btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Image = global::Navegador1.Properties.Resources.save;
+            this.btnSave.Location = new System.Drawing.Point(255, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(57, 54);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Image = global::Navegador1.Properties.Resources.search;
+            this.btnSearch.Location = new System.Drawing.Point(192, 3);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(57, 54);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Image = global::Navegador1.Properties.Resources.help;
+            this.btnHelp.Location = new System.Drawing.Point(129, 3);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(57, 54);
+            this.btnHelp.TabIndex = 2;
+            this.btnHelp.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Image = global::Navegador1.Properties.Resources.cancel;
+            this.btnCancel.Location = new System.Drawing.Point(66, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(57, 54);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Image = global::Navegador1.Properties.Resources.add;
+            this.btnAdd.Location = new System.Drawing.Point(3, 3);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(57, 54);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Navegador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnNext2);
+            this.Controls.Add(this.btnNext1);
+            this.Controls.Add(this.btnLast1);
+            this.Controls.Add(this.btnLast2);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Navegador";
+            this.Size = new System.Drawing.Size(630, 62);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnLast2;
+        private System.Windows.Forms.Button btnLast1;
+        private System.Windows.Forms.Button btnNext1;
+        private System.Windows.Forms.Button btnNext2;
+    }
+}
---
+++ Navegador1/Navegador1/Navegador.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Navegador1
+{
+    public partial class Navegador : UserControl
+    {
+
+        /*Create the events*/
+        public event EventHandler addButton;
+        public event EventHandler cancelButton;
+        public event EventHandler helpButton;
+        public event EventHandler searchButton;
+        public event EventHandler saveButton;
+        public event EventHandler printButton;
+        public event EventHandler last1Button;
+        public event EventHandler last2Button;
+        public event EventHandler next1Button;
+        public event EventHandler next2Button;
+
+
+        public Navegador()
+        {
+            InitializeComponent();
+        }
+
+
+
+        // 
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (this.addButton != null)
+            {
+                this.addButton(this, e);
+            }
+
+        }
+
+
+    }
+}
