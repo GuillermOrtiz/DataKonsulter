bffb2bf949255a75312636d036c09fcd8c5eef2f
JoseDavid1 <daviscx123@gmail.com>
2018-02-20 23:31:47 -0600

    OBJETO COMUN REPORTEADOR

---
+++ PruebaReporteador/PruebaReporteador/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace PruebaReporteador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vistaPreviaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imprimirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mantenimientoDeReportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listadoReportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.navegador1 = new Navegador.Navegador();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reportesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(702, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // reportesToolStripMenuItem
+            // 
+            this.reportesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opcionesToolStripMenuItem,
+            this.mantenimientoDeReportesToolStripMenuItem,
+            this.listadoReportesToolStripMenuItem});
+            this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
+            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.reportesToolStripMenuItem.Text = "Reportes";
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            this.opcionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vistaPreviaToolStripMenuItem,
+            this.imprimirToolStripMenuItem});
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            this.opcionesToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.opcionesToolStripMenuItem.Text = "Opciones";
+            // 
+            // vistaPreviaToolStripMenuItem
+            // 
+            this.vistaPreviaToolStripMenuItem.Name = "vistaPreviaToolStripMenuItem";
+            this.vistaPreviaToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.vistaPreviaToolStripMenuItem.Text = "Vista Previa ";
+            this.vistaPreviaToolStripMenuItem.Click += new System.EventHandler(this.vistaPreviaToolStripMenuItem_Click);
+            // 
+            // imprimirToolStripMenuItem
+            // 
+            this.imprimirToolStripMenuItem.Name = "imprimirToolStripMenuItem";
+            this.imprimirToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.imprimirToolStripMenuItem.Text = "Imprimir";
+            this.imprimirToolStripMenuItem.Click += new System.EventHandler(this.imprimirToolStripMenuItem_Click);
+            // 
+            // mantenimientoDeReportesToolStripMenuItem
+            // 
+            this.mantenimientoDeReportesToolStripMenuItem.Name = "mantenimientoDeReportesToolStripMenuItem";
+            this.mantenimientoDeReportesToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.mantenimientoDeReportesToolStripMenuItem.Text = "Mantenimiento de Reportes";
+            this.mantenimientoDeReportesToolStripMenuItem.Click += new System.EventHandler(this.mantenimientoDeReportesToolStripMenuItem_Click);
+            // 
+            // listadoReportesToolStripMenuItem
+            // 
+            this.listadoReportesToolStripMenuItem.Name = "listadoReportesToolStripMenuItem";
+            this.listadoReportesToolStripMenuItem.Size = new System.Drawing.Size(221, 22);
+            this.listadoReportesToolStripMenuItem.Text = "Listado Reportes";
+            this.listadoReportesToolStripMenuItem.Click += new System.EventHandler(this.listadoReportesToolStripMenuItem_Click);
+            // 
+            // navegador1
+            // 
+            this.navegador1.Location = new System.Drawing.Point(21, 48);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(559, 47);
+            this.navegador1.sNombreBD = "bd_seguridad";
+            this.navegador1.sNombreTabla = null;
+            this.navegador1.sPass = null;
+            this.navegador1.sResult = null;
+            this.navegador1.sServidor = null;
+            this.navegador1.sUsuario = null;
+            this.navegador1.TabIndex = 2;
+            this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 478);
+            this.Controls.Add(this.navegador1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem reportesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vistaPreviaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imprimirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mantenimientoDeReportesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listadoReportesToolStripMenuItem;
+        private Navegador.Navegador navegador1;
+    }
+}
+
---
+++ PruebaReporteador/PruebaReporteador/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dllconexion;
+using AdministradorDeReportes;
+
+
+namespace PruebaReporteador
+{
+    public partial class Form1 : Form
+    {
+        private static int rVista = 1;
+        private static int numeroAplicacion = 3000;
+
+        public Form1()
+        {
+            InitializeComponent();            
+            navegador1.IdApp = 1;
+        }
+
+        private void navegador1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vistaPreviaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            navegador1.VistaReporte = 1;
+        }
+
+        private void imprimirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            navegador1.VistaReporte = 0;
+        }
+
+        private void mantenimientoDeReportesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AdministradorDeReportes.Objetos.dbConexion.dbName = "bd_seguridad";
+            AdministradorDeReportes.Objetos.dbConexion.dbServer = "localhost";
+            AdministradorDeReportes.Objetos.dbConexion.dbUser = "root";
+            AdministradorDeReportes.Objetos.dbConexion.dbPass = "";
+            Mantenimiento rMante = new Mantenimiento();
+            rMante.Show();
+        }
+
+        private void listadoReportesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try {
+                dllconexion.gReport.establecerParametrosDB("localhost", "root", "bd_seguridad", "");
+                dllconexion.seleccionPath rLista = new dllconexion.seleccionPath(navegador1.IdApp, 0);
+                rLista.Show();
+            }
+            catch(Exception ex) { MessageBox.Show(ex.Message, "Error Reportes."); }            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
